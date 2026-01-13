9e578252fc9aaeac7aaca37521db6b535826d850
Zev Landau <creativeprojectsny+macbook@gmail.com>
2026-01-13 13:18:19 -0500

    feat: Updated some project config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Strict React mode is on by default in 16, but good to be explicit
+  reactStrictMode: true,
+  // Ensure we can use Google Maps script
+  transpilePackages: ['@react-google-maps/api'],
+  experimental: {
+    // optimizePackageImports: ['lucide-react'],
+  },
 };
 
 export default nextConfig;
